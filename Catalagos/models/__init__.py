c1d7b1f1849a46db7f83c192fc3a3b728ab5f0ad
Jesus Adrian <jesusadrianjeronimo113@gmail.com>
2024-04-20 23:25:40 -0600

    cat_escolaridades

--- Catalagos/models/__init__.py
+++ Catalagos/models/__init__.py
@@ -1,1 +1,2 @@
 from .cat_regiones import *
+from .cat_escolaridades import *
---
+++ Catalagos/models/cat_escolaridades.py
@@ -0,0 +1,15 @@
+from django.db import models
+from simple_history.models import HistoricalRecords
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+class cat_escolaridades(TimeStampedModel):
+    nombre = models.CharField(max_length=25)
+    history = HistoricalRecords()
+
+
+    def __str__(self):
+        return self.nombre
