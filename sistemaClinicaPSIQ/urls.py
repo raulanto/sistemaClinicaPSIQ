cedc31fa868231d3b401de25e968a070d8602883
raul <74162376+raulanto@users.noreply.github.com>
2024-04-22 20:51:58 -0600

    raíz al admin
     # Redirige la raíz al admin

--- sistemaClinicaPSIQ/urls.py
+++ sistemaClinicaPSIQ/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/', permanent=False)),  # Redirige la raíz al admin
     path('admin/', admin.site.urls),
+    # Tus otras rutas aquí
 ]
