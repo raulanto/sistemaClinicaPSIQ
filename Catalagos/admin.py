f3a65ff85595e6b08ec5da1429837cd305319653
raul <74162376+raulanto@users.noreply.github.com>
2024-04-22 21:41:47 -0600

    Vista admin avanzada

--- Catalagos/admin.py
+++ Catalagos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(cat_pais)
+# admin.site.register(cat_estados)
+
+@admin.register(cat_estados)
+class Estados(admin.ModelAdmin):
+
+    list_display = ['id','nombre','fk_cat_pais']
+    list_display_links = ['id']
+    list_filter = ['fk_cat_pais']
+
