7e6d9d1570487e36441387e5b065fa5c2cf4656e
raul <74162376+raulanto@users.noreply.github.com>
2024-04-22 21:41:17 -0600

    catalagos corregidos

--- Catalagos/models/__init__.py
+++ Catalagos/models/__init__.py
@@ -1,6 +1,6 @@
-from .cat_regiones import *
+from .cat_religiones import *
 from .cat_escolaridades import *
 from .cat_estados import *
 from .cat_municipios import *
 from .cat_tipo_asentamientos import *
-
+from .cat_pais import *
--- Catalagos/models/cat_estados.py
+++ Catalagos/models/cat_estados.py
@@ -1,5 +1,6 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
+from .cat_pais import cat_pais
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -9,6 +10,7 @@ class TimeStampedModel(models.Model):
 
 class cat_estados(TimeStampedModel):
     nombre = models.CharField(max_length=45)
+    fk_cat_pais= models.ForeignKey(cat_pais,on_delete=models.CASCADE,blank=True,null=True)
     history = HistoricalRecords()
 
 
