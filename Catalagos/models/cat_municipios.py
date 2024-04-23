3f5440b861421b8507f481057a835e60b3ad2d22
Jesus Adrian <jesusadrianjeronimo113@gmail.com>
2024-04-22 20:38:53 -0600

    cat_municipios

---
+++ Catalagos/models/cat_municipios.py
@@ -0,0 +1,16 @@
+from django.db import models
+from simple_history.models import HistoricalRecords
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class cat_municipios(TimeStampedModel):
+    nombre = models.CharField(max_length=45)
+    history = HistoricalRecords()
+
+
+    def __str__(self):
+        return self.nombre
